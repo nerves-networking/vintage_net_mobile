055f8fe2acb8eeca0fd1ab9cefb1ac8ed645ed88
Frank Hunleth <fhunleth@troodon-software.com>
2020-01-15 14:43:33 -0500

    Copy standard content to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,33 +1,80 @@
 defmodule VintageNetLTE.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/nerves-networking/vintage_net_lte"
+
   def project do
     [
       app: :vintage_net_lte,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.8",
+      test_coverage: [tool: ExCoveralls],
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
       compilers: [:elixir_make | Mix.compilers()],
       make_targets: ["all"],
       make_clean: ["clean"],
-      make_error_message: ""
+      make_error_message: "",
+      deps: deps(),
+      dialyzer: dialyzer(),
+      docs: docs(),
+      package: package(),
+      description: description()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {VintageNetLTE.Application, []}
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp description do
+    "LTE support for VintageNet"
+  end
+
+  defp package do
+    %{
+      files: [
+        "lib",
+        "test",
+        "mix.exs",
+        "Makefile",
+        "README.md",
+        "src/*.[ch]",
+        "src/test-c99.sh",
+        "LICENSE",
+        "CHANGELOG.md"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    }
+  end
+
   defp deps do
     [
       {:elixir_make, "~> 0.6", runtime: false},
       {:vintage_net, "~> 0.7"},
-      {:muontrap, "~> 0.5.0"}
+      {:muontrap, "~> 0.5.0"},
+      {:ex_doc, "~> 0.19", only: :docs, runtime: false},
+      {:excoveralls, "~> 0.8", only: :test, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.6", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp dialyzer() do
+    [
+      flags: [:race_conditions, :unmatched_returns, :error_handling, :underspecs]
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 end
