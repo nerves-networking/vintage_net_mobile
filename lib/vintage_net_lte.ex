78a8bdc0c1ff7817759ee55cb122a14f4575b01d
Matt Ludwigs <mattludwigs@gmail.com>
2020-01-16 15:51:31 -0700

    Refactor how pppd is called

--- lib/vintage_net_lte.ex
+++ lib/vintage_net_lte.ex
@@ -11,24 +11,7 @@ defmodule VintageNetLTE do
 
   @impl true
   def to_raw_config(ifname, %{type: __MOUDLE__} = config, opts) do
-    _ = Logger.error("PPP0!!!!!!!!!!!")
-    pppd = Keyword.fetch!(opts, :bin_pppd)
-    chat = Keyword.fetch!(opts, :bin_chat)
-    tmpdir = Keyword.fetch!(opts, :tmpdir)
-
-    # TODO: make this a config item
-    serial_port = "/dev/ttyUSB0"
-
-    # TODO: make this a config item
-    # should be allowed to pass in integer
-    # we can normalize the integer into a
-    # string
-    serial_speed = "115200"
-
-    # TODO: make service provider configurable
-    chatscript_path = Path.join(tmpdir, "#{Twilio.name()}")
-
-    files = [{chatscript_path, Twilio.chatscript()}]
+    files = [{chatscript_path(opts), Twilio.chatscript()}]
 
     # TODO: up command may differ between modems
     # should make this configurable
@@ -37,17 +20,9 @@ defmodule VintageNetLTE do
       {:fun, __MODULE__, :run_mknod, []}
     ]
 
-    child_specs = [
-      {VintageNetLTE.PPPD,
-       [
-         chatscript: chatscript_path,
-         pppd: pppd,
-         chat: chat,
-         ifname: ifname,
-         speed: serial_speed,
-         serial: serial_port
-       ]}
-    ]
+    child_specs = [make_pppd_spec(opts)]
+
+    Logger.error("#{inspect(child_specs)}")
 
     %RawConfig{
       ifname: ifname,
@@ -67,12 +42,46 @@ defmodule VintageNetLTE do
   @impl true
   def check_system(_), do: :ok
 
-  def chat_script_path() do
-    Path.join(System.tmp_dir!(), "twilio")
+  defp make_pppd_spec(opts) do
+    pppd_bin = Keyword.fetch!(opts, :bin_pppd)
+    priv_dir = Application.app_dir(:vintage_net_lte, "priv")
+    pppd_shim_path = Path.join(priv_dir, "pppd_shim.so")
+    pppd_args = make_pppd_args(opts)
+
+    env = [{"PRIV_DIR", priv_dir}, {"LD_PRELOAD", pppd_shim_path}]
+
+    {MuonTrap.Daemon, [pppd_bin, pppd_args, [env: env]]}
   end
 
-  def write_chat_script() do
-    :ok = File.write(chat_script_path(), Twilio.chatscript())
+  defp make_pppd_args(opts) do
+    chat_bin = Keyword.fetch!(opts, :bin_chat)
+    cs_path = chatscript_path(opts)
+    # TODO: make this a config item
+    serial_port = "/dev/ttyUSB0"
+
+    # TODO: make this a config item
+    # should be allowed to pass in integer
+    # we can normalize the integer into a
+    # string
+    serial_speed = "115200"
+
+    [
+      "connect",
+      "#{chat_bin} -v -f #{cs_path}",
+      serial_port,
+      serial_speed,
+      "noipdefault",
+      "usepeerdns",
+      "persist",
+      "noauth",
+      "debug"
+    ]
+  end
+
+  defp chatscript_path(opts) do
+    opts
+    |> Keyword.fetch!(:tmpdir)
+    |> Path.join(Twilio.name())
   end
 
   def run_mknod() do
@@ -82,38 +91,8 @@ defmodule VintageNetLTE do
   end
 
   def run_usb_modeswitch() do
-    {_, 0} = System.cmd("usb_modeswitch", ["-v", "12d1", "-p", "14fe", "-J"])
+    _ = System.cmd("usb_modeswitch", ["-v", "12d1", "-p", "14fe", "-J"])
     :timer.sleep(1_000)
     :ok
   end
-
-  def setup() do
-    :ok = write_chat_script()
-    :ok = run_mknod()
-    :ok = run_usb_modeswitch()
-  end
-
-  def run_pppd(serial_port, opts \\ []) do
-    speed = Keyword.get(opts, :speed, "115200")
-    debug = Keyword.get(opts, :debug, false)
-
-    pppd_opts =
-      [
-        "connect",
-        "chat -v -f #{chat_script_path()}",
-        serial_port,
-        speed,
-        "noipdefault",
-        "usepeerdns",
-        #        "defaultroute",
-        "noauth",
-        "persist"
-      ]
-      |> add_debug_to_opts(debug)
-
-    MuonTrap.Daemon.start_link("pppd", pppd_opts)
-  end
-
-  defp add_debug_to_opts(pppd_opts, true), do: pppd_opts ++ ["debug"]
-  defp add_debug_to_opts(pppd_opts, false), do: pppd_opts
 end
