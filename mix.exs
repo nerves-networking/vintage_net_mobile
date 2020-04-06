d902bebec497b4207f6594cc81da07c28cdd295e
Frank Hunleth <fhunleth@troodon-software.com>
2020-04-06 18:48:01 -0400

    Bump allowed version of muontrap

--- mix.exs
+++ mix.exs
@@ -62,8 +62,8 @@ defmodule VintageNetMobile.MixProject do
   defp deps do
     [
       {:elixir_make, "~> 0.6", runtime: false},
-      {:vintage_net, "~> 0.7.6"},
-      {:muontrap, "~> 0.5.0"},
+      {:vintage_net, "~> 0.7.8"},
+      {:muontrap, "~> 0.5.0 or ~> 0.6.0"},
       {:ex_doc, "~> 0.19", only: :docs, runtime: false},
       {:excoveralls, "~> 0.8", only: :test, runtime: false},
       {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
