f49a9a1b680308aa40e01e4bc31571c5c2ca04b8
Frank Hunleth <fhunleth@troodon-software.com>
2020-04-14 12:39:33 -0400

    Force vintage_net v0.7.9 or later
    This pulls in the PPP IP address fix. Without it, the IP address for the
    PPP link that shows up in the properties is the wrong side of the
    connection.

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule VintageNetMobile.MixProject do
   defp deps do
     [
       {:elixir_make, "~> 0.6", runtime: false},
-      {:vintage_net, "~> 0.7.8"},
+      {:vintage_net, "~> 0.7.9"},
       {:muontrap, "~> 0.5.0 or ~> 0.6.0"},
       {:ex_doc, "~> 0.19", only: :docs, runtime: false},
       {:excoveralls, "~> 0.8", only: :test, runtime: false},
