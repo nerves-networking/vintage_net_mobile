c4856bca5625b06b7f204c38db1220725074682c
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-02-24 15:09:51 -0700

    Add support for Telit LE910 modem

---
+++ lib/vintage_net_mobile/modem/telit_LE910.ex
@@ -0,0 +1,89 @@
+defmodule VintageNetMobile.Modem.TelitLE910 do
+  @behaviour VintageNetMobile.Modem
+
+  @moduledoc """
+  Telit LE910 support
+
+  ```elixir
+  VintageNet.configure(
+    "ppp0",
+    %{
+      type: VintageNetMobile,
+      vintage_net_mobile: %{
+        modem: VintageNetMobile.Modem.TelitLE910,
+        service_providers: [%{apn: "wireless.twilio.com"}]
+      }
+    }
+  )
+  ```
+
+  If multiple service providers are configured, this implementation only
+  attempts to connect to the first one.
+
+  Example of supported properties
+
+  ```elixir
+  iex> VintageNet.get_by_prefix ["interface", "ppp0"]
+  [
+  {["interface", "ppp0", "addresses"],
+   [
+     %{
+       address: {100, 79, 181, 147},
+       family: :inet,
+       netmask: {255, 255, 255, 255},
+       prefix_length: 32,
+       scope: :universe
+     }
+   ]},
+  {["interface", "ppp0", "config"],
+   %{
+     type: VintageNetMobile,
+     vintage_net_mobile: %{
+       modem: VintageNetMobile.Modem.TelitLE910,
+       service_providers: [%{apn: "super"}, %{apn: "wireless.twilio.com"}]
+     }
+   }},
+  {["interface", "ppp0", "connection"], :internet},
+  {["interface", "ppp0", "hw_path"], "/devices/virtual"},
+  {["interface", "ppp0", "lower_up"], true},
+  {["interface", "ppp0", "mobile", "cid"], 123098825},
+  {["interface", "ppp0", "mobile", "lac"], 32773},
+  {["interface", "ppp0", "mobile", "signal_4bars"], 3},
+  {["interface", "ppp0", "mobile", "signal_asu"], 19},
+  {["interface", "ppp0", "mobile", "signal_dbm"], -75},
+  {["interface", "ppp0", "present"], true},
+  {["interface", "ppp0", "state"], :configured},
+  {["interface", "ppp0", "type"], VintageNetMobile}
+  ]
+  ```
+  """
+
+  alias VintageNet.Interface.RawConfig
+  alias VintageNetMobile.{Chatscript, ExChat, PPPDConfig, SignalMonitor}
+
+  @impl VintageNetMobile.Modem
+  def normalize(config) do
+    config
+  end
+
+  @impl VintageNetMobile.Modem
+  def add_raw_config(raw_config, %{vintage_net_mobile: mobile} = _config, opts) do
+    ifname = raw_config.ifname
+
+    files = [{Chatscript.path(ifname, opts), Chatscript.default(mobile)}]
+    at_tty = Map.get(mobile, :at_tty, "ttyUSB2")
+    ppp_tty = Map.get(mobile, :ppp_tty, "ttyUSB3")
+
+    child_specs = [
+      {ExChat, [tty: at_tty, speed: 9600]},
+      {SignalMonitor, [ifname: ifname, tty: at_tty]}
+    ]
+
+    %RawConfig{
+      raw_config
+      | files: files,
+        child_specs: child_specs
+    }
+    |> PPPDConfig.add_child_spec(ppp_tty, 9600, opts)
+  end
+end
---
+++ test/vintage_net_mobile/modem/telit_le910_test.exs
@@ -0,0 +1,76 @@
+defmodule VintageNetMobile.Modem.TelitLE910Test do
+  use ExUnit.Case, async: true
+
+  alias VintageNetMobile.Modem.TelitLE910
+  alias VintageNet.Interface.RawConfig
+
+  test "generates LTE configuration" do
+    priv_dir = Application.app_dir(:vintage_net_mobile, "priv")
+
+    input = %{
+      type: VintageNetMobile,
+      vintage_net_mobile: %{
+        modem: TelitLE910,
+        service_providers: [%{apn: "choosethislteitissafe"}, %{apn: "wireless.twilio.com"}]
+      }
+    }
+
+    output = %RawConfig{
+      ifname: "ppp0",
+      type: VintageNetMobile,
+      source_config: input,
+      required_ifnames: ["wwan0"],
+      up_cmds: [
+        {:run_ignore_errors, "mknod", ["/dev/ppp", "c", "108", "0"]}
+      ],
+      down_cmds: [
+        {:fun, VintageNet.PropertyTable, :clear_prefix,
+         [VintageNet, ["interface", "ppp0", "mobile"]]}
+      ],
+      files: [
+        {"/tmp/vintage_net/chatscript.ppp0",
+         """
+         ABORT 'BUSY'
+         ABORT 'NO CARRIER'
+         ABORT 'NO DIALTONE'
+         ABORT 'NO DIAL TONE'
+         ABORT 'NO ANSWER'
+         ABORT 'DELAYED'
+         TIMEOUT 10
+         REPORT CONNECT
+         "" +++
+         "" AT
+         OK ATH
+         OK ATZ
+         OK ATQ0
+         OK AT+CGDCONT=1,"IP","choosethislteitissafe"
+         OK ATDT*99***1#
+         CONNECT ''
+         """}
+      ],
+      child_specs: [
+        {MuonTrap.Daemon,
+         [
+           "pppd",
+           [
+             "connect",
+             "chat -v -f /tmp/vintage_net/chatscript.ppp0",
+             "ttyUSB3",
+             "9600",
+             "noipdefault",
+             "usepeerdns",
+             "persist",
+             "noauth",
+             "nodetach",
+             "debug"
+           ],
+           [env: [{"PRIV_DIR", priv_dir}, {"LD_PRELOAD", Path.join(priv_dir, "pppd_shim.so")}]]
+         ]},
+        {VintageNetMobile.ExChat, [tty: "ttyUSB2", speed: 9600]},
+        {VintageNetMobile.SignalMonitor, [ifname: "ppp0", tty: "ttyUSB2"]}
+      ]
+    }
+
+    assert output == VintageNetMobile.to_raw_config("ppp0", input, Utils.default_opts())
+  end
+end
