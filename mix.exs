4d15daa2bd67182612df731e5ca17c655f79b35a
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-02-25 14:44:20 -0700

    v0.11.0 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule VintageNetMobile.MixProject do
   use Mix.Project
 
-  @version "0.10.4"
+  @version "0.11.0"
   @source_url "https://github.com/nerves-networking/vintage_net_mobile"
 
   def project do
